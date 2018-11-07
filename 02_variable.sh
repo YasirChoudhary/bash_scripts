7d096673849f27ddba728cdcef3ccdf1a3acfa86
YasirChoudhary <yasirchoudhary81@gmail.com>
2018-11-07 22:03:26 +0530

    added variable script

---
+++ 02_variable.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+
+# #! is called as shebang 
+# System Variable
+echo $BASH 
+echo my shell name is $BASH
+echo $BASH_VERSION
+echo $HOME
+echo $PWD
+
+# User defined variable
+name=Yasir
+echo $name
+echo The name is $name
+val=10
+echo value $val
